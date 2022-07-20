bc146d5c97b923ac270a80631e88f70c2c5d7527
Artur <artur.krupa@icloud.com>
2022-07-20 22:18:08 +0200

    Select and add event listener for tip buttons

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ const billInput = document.querySelector(".bill-input");
 const numOfPeopleInpit = document.querySelector(".num-of-ppl-input");
 const customTip = document.querySelector(".input-tip-custom");
 const resetButton = document.querySelector(".reset-btn");
+const tipButtons = document.querySelectorAll(".btn");
+
+for (const button of tipButtons) {
+  button.addEventListener("click", () => {
+    console.log(button);
+  });
+}
