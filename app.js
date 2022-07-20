afce8b7562c2a4a934b08b01272c9dc160dc39ca
Artur <artur.krupa@icloud.com>
2022-07-20 22:02:44 +0200

    Select inputs elements in Js

---
+++ app.js
@@ -0,0 +1,5 @@
+//Selecting Inputs
+const billInput = document.querySelector(".bill-input");
+const numOfPeopleInpit = document.querySelector(".num-of-ppl-input");
+const customTip = document.querySelector(".input-tip-custom");
+const resetButton = document.querySelector(".reset-btn");
