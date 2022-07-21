934287469b6987b5a4ca1a1cc04d38b43b26f386
Artur <artur.krupa@icloud.com>
2022-07-21 21:57:00 +0200

    Add totalPrice and tipPerPerson

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const numOfPeopleInpit = document.querySelector(".num-of-ppl-input");
 const customTip = document.querySelector(".input-tip-custom");
 const resetButton = document.querySelector(".reset-btn");
 const tipButtons = document.querySelectorAll(".btn");
+const totalPrice = document.querySelector(".total-price");
+const tipPerPerson = document.querySelector(".tip-per-person");
 
 for (const button of tipButtons) {
   button.addEventListener("click", () => {
