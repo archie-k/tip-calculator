a6037acbd10b3c296c790eb53232322d750ea97e
Artur <artur.krupa@icloud.com>
2022-07-25 22:49:21 +0200

    hotfix

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-//Selecting Inputs
+//Inputs
 const billInput = document.querySelector(".bill-input");
 const numOfPeopleInpit = document.querySelector(".num-of-ppl-input");
 const customTip = document.querySelector(".input-tip-custom");
-const resetButton = document.querySelector(".reset-btn");
 const tipButtons = document.querySelectorAll(".btn");
+
+const resetButton = document.querySelector(".reset-btn");
 const totalPrice = document.querySelector(".total-price");
 const tipPerPerson = document.querySelector(".tip-per-person");
 
+let selectedTip;
+let billAmount = console.log(billAmount);
+
 for (const button of tipButtons) {
   button.addEventListener("click", () => {
+    billInput.value;
     console.log(button);
   });
 }
+
+let calculateTip = () => {};
+
+let calculateTotal = () => {};
