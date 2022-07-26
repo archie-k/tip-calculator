fc2cf65101206019c3f9403a4c7345902979528b
Artur <artur.krupa@icloud.com>
2022-07-26 19:57:15 +0200

    Add event listeners to input and calc function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Inputs
 const billInput = document.querySelector(".bill-input");
-const numOfPeopleInpit = document.querySelector(".num-of-ppl-input");
+const numOfPeopleInput = document.querySelector(".num-of-ppl-input");
 const customTip = document.querySelector(".input-tip-custom");
 const tipButtons = document.querySelectorAll(".btn");
 
@@ -8,16 +8,30 @@ const resetButton = document.querySelector(".reset-btn");
 const totalPrice = document.querySelector(".total-price");
 const tipPerPerson = document.querySelector(".tip-per-person");
 
-let selectedTip;
-let billAmount = console.log(billAmount);
-
 for (const button of tipButtons) {
-  button.addEventListener("click", () => {
+  button.addEventListener("focus", () => {
     billInput.value;
     console.log(button);
   });
 }
 
-let calculateTip = () => {};
+let selectedTip;
+let billValue;
+let numberOfPeople;
 
-let calculateTotal = () => {};
+billInput.addEventListener("blur", () => {
+  billValue = Number(billInput.value);
+});
+
+numOfPeopleInput.addEventListener("blur", () => {
+  numberOfPeople = Number(numOfPeopleInput.value);
+});
+
+let calculateTip = () => {
+  let tip = (billInput.value * selectedTip) / numOfPeople;
+  return tip;
+};
+
+let calculateTotal = () => {
+  return;
+};
