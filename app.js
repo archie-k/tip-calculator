e6836c07f9836df9b45df38555d29b209563b7bb
Artur <artur.krupa@icloud.com>
2022-07-30 17:52:18 +0200

    Add logic to buttons and inputs

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Inputs
+//SELECTING INPUTS
 const billInput = document.querySelector(".bill-input");
 const numOfPeopleInput = document.querySelector(".num-of-ppl-input");
 const customTip = document.querySelector(".input-tip-custom");
@@ -8,27 +8,19 @@ const resetButton = document.querySelector(".reset-btn");
 const totalPrice = document.querySelector(".total-price");
 const tipPerPerson = document.querySelector(".tip-per-person");
 
-for (const button of tipButtons) {
-  button.addEventListener("click", () => {
-    billInput.value;
-    console.log(button);
-    button.style.color = "red";
-  });
-}
-
+//CALCULATE VARIABLES
 let selectedTip;
 let billValue;
 let numberOfPeople;
+let calculatedTip;
 
-let resetingInputs = () => {
-  billInput.value = "";
-  numOfPeopleInput.value = "";
-};
-
-let calculateTip = () => {
-  let tip = (billInput.value * selectedTip) / numOfPeople;
-  return tip;
-};
+//INSERTING VALUES
+for (const button of tipButtons) {
+  button.addEventListener("click", () => {
+    billInput.value;
+    selectedTip = Number(button.value.slice(0, button.value.length - 1));
+  });
+}
 
 billInput.addEventListener("blur", () => {
   billValue = Number(billInput.value);
@@ -36,11 +28,29 @@ billInput.addEventListener("blur", () => {
 
 numOfPeopleInput.addEventListener("blur", () => {
   numberOfPeople = Number(numOfPeopleInput.value);
-  console.log(numberOfPeople);
+  calculateTip();
+  calculateTotal();
 });
+
+//CALCULATION SECTION
+
+let calculateTip = () => {
+  let tip = (billInput.value * (selectedTip / 100)) / numberOfPeople;
+  calculatedTip = Number(tip.toPrecision(3));
+};
+
+let calculateTotal = () => {
+  let totalValue = billValue / numberOfPeople + calculatedTip;
+  tipPerPerson.textContent = calculatedTip;
+  totalPrice.textContent = totalValue;
+};
+
+let tipSelection = () => {};
 
 resetButton.addEventListener("click", resetingInputs);
 
-let calculateTotal = () => {
-  return;
+//Reset
+let resetInputs = () => {
+  billInput.value = "";
+  numOfPeopleInput.value = "";
 };
